58c58cec0b530061dc6fa55df4a3139f91849875
Michael Pedzimaz <mpedzi03@gmail.com>
2021-05-23 15:29:53 -0500

    react-food-cart-app
    up to modal section

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,11 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Delicious food, delivered to you!</h2>
+        <p>Choose your favorite meal from our wide variety of delicious goods!</p>
+        <p>All of our meals are cooked with the freshest and finest ingredients.</p>
+    </section>
+}
+
+export default MealsSummary
