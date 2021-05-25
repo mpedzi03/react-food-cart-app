9bbb94f1455cedef375d0826ae9cfd0e9f3b70ed
Michael Pedzimaz <mpedzi03@gmail.com>
2021-05-24 20:28:33 -0500

    react-food-cart
    useContext work

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,35 @@
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
 
 const Cart = props => {
-    const cartItems = (
-        <ul className={classes['cart-items']}>
-            {[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}
-        </ul>)
-    return (
-        <Modal onHideCart={props.onHideCart}>
-            {cartItems}
-            <div>
-                <span>Total Amount</span>
-                <span>35.62</span>
-            </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-                <button className={classes.button}>Order</button>
-            </div>
-        </Modal>
-    )
+  const cartCtx = useContext(CartContext)
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+  const hasItems = cartCtx.items.length > 0
+
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  )
+
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {cartItems}
+      <div>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  )
 }
 
 export default Cart
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -5,7 +5,11 @@ import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
-  useContext(CartContext)
+  const cartCtx = useContext(CartContext)
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0)
 
   return (
     <button className={classes.button} onClick={props.onShowCart}>
@@ -13,9 +17,7 @@ const HeaderCartButton = props => {
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
